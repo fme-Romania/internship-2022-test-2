b9a76c7c002fca93f0ad2462cbd2ed447193e843
EmiBrd <emi.bordeianu.1999@gmail.com>
2022-05-26 13:53:24 +0300

    Commit test FME

--- test-internship-p2/src/fme/internship/Test2.java
+++ test-internship-p2/src/fme/internship/Test2.java
@@ -25,14 +25,56 @@
 
 package fme.internship;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * 
- * @author <full name>
- * @email <email address>
+ * @author <Emanuel Bordeianu>
+ * @email <emi.bordeianu.1999@gmail.com>
  * 
  */
 public class Test2 {
+
+    static void printDistinctWords(List<String> stringList){
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String s: stringList){
+            if(map.containsKey(s))
+                map.put(s, map.get(s) + 1);
+            else
+                map.put(s, 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1)
+                System.out.println(entry.getKey());
+        }
+    }
+
     public static void main(String[] args) {
+
         System.out.println("FME Internship - Test2");
+        List<String> stringList = new ArrayList<>();
+
+        try {
+            File myObj = new File("test-internship-p2\\src\\test.in");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                //String data = myReader.nextLine();
+                String data = myReader.next();
+                //System.out.println(data);
+                stringList.add(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+        printDistinctWords(stringList);
+
     }
 }
